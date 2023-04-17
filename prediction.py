e83c22fd726c6f6dfbe15e42f6c9b808182148f4
Krrish <malekm09122003@gmail.com>
2023-04-18 00:23:42 +0530

    Added prediction.py for easier accessibility of the prediction model through the backend server

---
+++ prediction.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+def predict(vaccine,state,year):
+    df = pd.read_csv("./Web Scraper/compiled_report.csv")
+    i=0
+    initial=-1
+    final=-1
+    for index, row in df.iterrows():
+        if(row['VACCINE']==vaccine and row['STATE']==state):
+            if(initial==-1): initial=i
+            final=i+1
+        elif(final!=-1): break
+        i+=1
+    df = df.iloc[initial:final]
+    df = df.drop("STATE",axis = 1)
+    df = df.drop("VACCINE",axis = 1)
+    x = df["YEAR"]
+    y = df["APPLICANTS"]
+    z = df["VACCINATED"]
+    inp = np.copy(x)
+    inp = inp.reshape(len(x),1)
+    output_cases = np.copy(y)
+    output_vaccinated = np.copy(z)
+    np.random.seed(42)
+    x_train,x_test,y_train,y_test = train_test_split(inp,output_cases,test_size = 1)
+    x1_train,x1_test,y1_train,y1_test = train_test_split(inp,output_vaccinated,test_size = 1)
+    reg1 = LinearRegression().fit(x_train, y_train)#model for cases
+    reg2 = LinearRegression().fit(x1_train, y1_train)#model for vaccinated
+    x=x.to_list()
+    y=y.to_list()
+    z=z.to_list()
+    prediction_data = {}
+    last_year = x[-1]
+    for i in range (last_year+1,year+1):
+        cases = int(np.trunc(reg1.predict([[i]])))
+        prediction_data[i] = [cases]
+        x.append(i)
+        y.append(cases)
+    yearly_increase = int((prediction_data[year][0]-z[-1])/(year-last_year))
+    itr = z[-1]
+    for i in range(last_year+1,year+1):
+        itr += yearly_increase
+        z.append(itr)
+        prediction_data[i].append(itr)
+    meet = "Infinity"
+    for i in range(last_year+1,3000):
+        curr_cases = int(np.trunc(reg1.predict([[i]]))) 
+        curr_vacc = int(np.trunc(reg2.predict([[i]])))
+        if(curr_vacc>=curr_cases):
+            meet = i
+    fig,ax = plt.subplots(figsize=(8,5))
+    x = np.array(x)
+    ax.plot(x,y)
+    ax.plot(x,z)
+    locator = matplotlib.ticker.MultipleLocator(2)
+    plt.gca().xaxis.set_major_locator(locator)
+    formatter = matplotlib.ticker.StrMethodFormatter("{x:.0f}")
+    plt.gca().xaxis.set_major_formatter(formatter)
+    ax.set(title = f"{state} report of {vaccine}",xlabel = "Year",ylabel = "Cases every year")
+    plt.savefig('graph.png')
+    return {"data":prediction_data,"meet":meet}
